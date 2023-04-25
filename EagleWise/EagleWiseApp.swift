28ed0642444e1c71b4e3eb024cb79688e2d4a347
Dylan <dylan96514@gmail.com>
2023-04-25 14:03:47 -0400

    Initial Commit

---
+++ EagleWise/EagleWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  EagleWiseApp.swift
+//  EagleWise
+//
+//  Created by Dylan Kim on 2023/04/25.
+//
+
+import SwiftUI
+
+@main
+struct EagleWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
